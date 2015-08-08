30ab7bb4465a6884c30e8f37a1cbd745b85780a3
justinsa <justinsa@live.ca>
2015-08-08 04:20:01 -0700

    Pagination support
    * Pagination is now fully operational.

--- client/js/controllers/home.controller.js
+++ client/js/controllers/home.controller.js
@@ -6,11 +6,41 @@ angular.module('application.controllers')
       from: 0,
       limit: 25,
       makes: [],
-      recalls: [],
-      start: undefined,
+      start: undefined
+    };
+    $scope.page = {
+      current: 0,
+      options: _.assign({}, $scope.controls),
       total: 0
     };
-    $scope.results = {};
+    $scope.results = {
+      recalls: []
+    };
+
+    $scope.actions = {
+      search: function () {
+        $scope.page.current = 1;
+        _.assign($scope.page.options, $scope.controls);
+
+        data.api.recalls.count($scope.page.options).then(function (total) {
+          $scope.page.total = total.count;
+        });
+
+        data.api.recalls.get($scope.page.options).then(function (recalls) {
+          $scope.results.recalls.length = 0;
+          $.merge($scope.results.recalls, recalls);
+        });
+      },
+
+      page: function () {
+        $scope.page.options.from = ($scope.page.current - 1) * $scope.page.options.limit;
+
+        data.api.recalls.get($scope.page.options).then(function (recalls) {
+          $scope.results.recalls.length = 0;
+          $.merge($scope.results.recalls, recalls);
+        });
+      }
+    };
 
     data.api.years.get().then(function (years) {
       $scope.results.years = years;
@@ -20,43 +50,5 @@ angular.module('application.controllers')
     });
     data.api.makes.get().then(function (makes) {
       $scope.results.makes = makes;
-    });
-
-    $scope.actions = {
-      search: function () {
-        $scope.page = {
-          options: _.pick($scope.controls, 'end', 'limit', 'makes', 'start')
-        };
-        $scope.page.options.from = 0;
-        $scope.page.current = 1;
-
-        data.api.recalls.count($scope.page.options).then(function (total) {
-          $scope.page.total = total;
-        });
-
-        data.api.recalls.get($scope.page.options).then(function (recalls) {
-          $scope.results.recalls = recalls;
-        });
-      },
-
-      page: function (number) {
-        var total = $scope.page.total,
-            limit = $scope.page.options.limit,
-            pages = total / limit + (total % limit > 0 ? 1 : 0);
-        number = (number < 1) ? 1 : number;
-        number = (number > pages) ? pages : number;
-        $scope.page.options.from = (number - 1) * limit;
-
-        data.api.recalls.get($scope.page.options).then(function (recalls) {
-          $scope.results.recalls = recalls;
-        });
-      }
-    };
-
-    $scope.$watch('page.current', function (newValue, oldValue) {
-      if (newValue === oldValue || !_.isNumber(newValue)) {
-        return;
-      }
-      $scope.actions.page(newValue);
     });
   }]);
